964430ad369798c72b42f60392a94c04c83e4644
Mohammed Niyas <niyask1997@gmail.com>
2022-08-31 16:24:01 +0530

    personal website updating

---
+++ portfolio.js
@@ -0,0 +1,80 @@
+// Header background change
+
+function scrollHeader(){
+    const header=document.getElementById('header');
+    if(this.scrollY>=50)header.classList.add('scroll-header');
+    else header.classList.remove(scroll-header);
+}
+window.addEventListener('scroll',scrollHeader);
+
+//================= SERVICES MODEL=========
+
+var modal=document.querySelectorAll('.services-model');
+var btn=document.querySelectorAll('.services-button');
+var close=document.querySelectorAll('.services_model-close');
+
+let modaladd=function(modalClick){
+    modal[modalClick].classList.add('active-modal')
+}
+
+btn.forEach((mb,i)=>{
+    mb.addEventListener('click',()=>{
+        modaladd(i)
+    })
+})
+close.forEach((mc)=>{
+    mc.addEventListener('click',()=>{
+        modal.forEach((m)=>{
+            m.classList.remove('active-modal')
+        })
+    })
+    
+})
+
+//========== SCROLL SECTION ACTIVE============//
+
+let section = document.querySelectorAll('section');
+        let lists = document.querySelectorAll(".nav-item");
+        function activeLink(li) {
+            lists.forEach((item) => item.classList.remove('active-link'));
+            li.classList.add('active-link');
+        }
+        lists.forEach((item) =>
+            item.addEventListener('click', function(){
+                activeLink(this);
+            }));
+
+        window.onscroll = () => {
+            section.forEach(section => {
+                let top = window.scrollY;
+                let offset = section.offsetTop;
+                let height = section.offsetHeight;
+                let id = section.getAttribute('id');
+
+                if (top >= offset && top < offset + height) {
+                    let target = document.querySelector(`[href='#${id}']`).parentElement;
+                    activeLink(target);
+                }
+            })
+        };
+
+        // Light Dark Theme //
+        function myFunction() {
+            var element = document.body;
+            element.classList.toggle("light-theme");
+         }
+
+        //  Scroll reveal Animation
+        const sr=ScrollReveal(
+            {
+                origin:'top',
+                distance:'60px',
+                duration:2500,
+                delay:400
+            }
+        )
+        sr.reveal('.home-data')
+        sr.reveal('.image-handler',{delay:700})
+        sr.reveal('.home-social,.home-scroll',{delay:900,origin:'bottom'})
+
+         
